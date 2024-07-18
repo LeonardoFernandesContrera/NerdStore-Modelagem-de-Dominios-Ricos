2923ad7fa05e234b9d2563c030aae7d6e5b6139e
Leonardo Fernandes Contrera <leonardo.contrera@bkbank.com.br>
2024-07-18 10:00:37 -0300

    feat : Persistencia com Repositorios

---
+++ src/NerdStore.Catalogo.Data/Mappings/CategoriaMapping.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Data.Mappings
+{
+    public class CategoriaMapping : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            builder.HasKey(cat => cat.Id);
+
+            builder.Property(cat => cat.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(250)");
+
+            //1 : N => Categorias : Produtos
+
+            builder.HasMany(cat => cat.Produtos)
+                .WithOne(prod => prod.Categoria)
+                .HasForeignKey(prod => prod.CategoriaId);
+
+            builder.ToTable("Categorias");
+        }
+    }
+}
---
+++ src/NerdStore.Catalogo.Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Data.Mappings
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(prod => prod.Id);
+
+            builder.Property(prod => prod.Nome)
+               .IsRequired()
+               .HasColumnType("varchar(250)");
+
+            builder.Property(prod => prod.Descricao)
+               .IsRequired()
+               .HasColumnType("varchar(500)");
+
+            builder.Property(prod => prod.Imagem)
+               .IsRequired()
+               .HasColumnType("varchar(250)");
+
+            builder.OwnsOne(prod => prod.Dimensoes, cm =>
+            {
+                cm.Property(c => c.Altura)
+                .HasColumnName("Altura")
+                .HasColumnType("int");
+
+                cm.Property(c => c.Largura)
+                .HasColumnName("Largura")
+                .HasColumnType("int");
+
+                cm.Property(c => c.Profundidade)
+               .HasColumnName("Profundidade")
+               .HasColumnType("int");
+            });
+
+            builder.ToTable("Produtos");
+        }
+    }
+}
